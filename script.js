ae18e57eaea5ae558922a1d4203c3f8a9bde5f4d
dagobert renouf <dagobert.king@gmail.com>
2014-04-04 14:48:18 +0200

    Load inner-content with pjax when navigating

--- script.js
+++ script.js
@@ -3,10 +3,54 @@ $(document).ready( function(){
 
 });
 
-$(document).pjax('nav a', 'article.container');
+$(document).pjax('#pjax-nav a', '#pjax-container', {
+	fragment: "#pjax-container"
+});
+
+$(document).on('pjax:click', function(event) {
+  
+  console.log('pjax click!');
+
+})
 
 $(document).on('pjax:start', function(event) {
   
-  console.log('pjax!');
+  console.log('pjax starts!');
 
+});
+
+$(document).on('pjax:end', function(event) {
+  
+  console.log('pjax ends!');
+
+});
+
+$(document).on('pjax:send', function(event) {
+
+	console.log('request send');
+
+});
+
+$(document).on('pjax:complete', function(event) {
+
+	console.log('request complete');
+
+});
+
+$(document).on('pjax:success', function(event) {
+
+	console.log('request succeeds');
+
+});
+
+$(document).on('pjax:error', function(event) {
+  
+  console.log('pjax error');
+
+});
+
+
+$(document).on('pjax:timeout', function(event) {
+  // Prevent default timeout redirection behavior
+  event.preventDefault();
 })
