d0a37fcdadafdb5dc2a8364c1140559adbb19559
dagobert renouf <dagobert.king@gmail.com>
2014-04-05 14:32:19 +0200

    Deactive links while transitionning between pages

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+// elements declaration
+var navEltsClass = '#pjax-nav a';
+var navElts = $(navEltsClass);
+var curNavEltId = '';
+
 var containerClass = '#pjax-container';
 var container = $(containerClass);
+
 var cloneClass = 'clone';
 var transitionClass = 'fade';
 
-$(document).pjax('#pjax-nav a', container, {
+// initiating pjax
+$(document).pjax(navEltsClass, container, {
 
-	fragment: containerClass
+	// what part of html file we should get
+	fragment: containerClass 
 
 });
 
@@ -13,28 +21,93 @@ $(document).on('pjax:start', function(event) {
   
 	console.log('pjax starts!');
 
-	container.clone().insertAfter(container).addClass(cloneClass); // duplicates content and put it right above container (through position absolute and z-index)
+	curNavEltId = event.relatedTarget.id;
+
+	disableNav();
+
+	container.clone().insertAfter(container).addClass(cloneClass); // duplicates content and put it right above container for disappearance effect (through position absolute and z-index)
 
 });
 
-$(document).on('pjax:send', function(event) {
+$(document).on('pjax:success', function(event) {
 
-	console.log('pjax sends request!');
+	console.log('pjax success');
 
-	setTimeout(function(){ // add transition class that makes clone disappear on top of new content
+	setTimeout(function(){ // add transition class that starts visual transition
+		
+		console.log('transition starts');
+		
 		$('.' + cloneClass).addClass(transitionClass);
-	}, 50); // adding 1ms delay to ensure proper firing of css transition
+
+	}, 50); // adding 50ms delay to ensure proper firing of css transition
 
 	// once transition is complete, remove the clone element
 	$('.' + cloneClass).bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(){
 
+		console.log('transition ends');
+
 		$('.' + cloneClass).remove();
+
+		reactivateNav();
 
 	});
 
 });
 
 
+// disable nav links
+function disableNav(){
+
+	// should disable pjax on these altogether, maybe rename the class?
+	navElts.each(function(){
+
+		$(this).attr('data-url', $(this).attr('href'));
+		$(this).removeAttr('href');
+		navActive = false;
+
+	})
+
+}
+// reactivate nav links
+function reactivateNav(){
+
+	navElts.each(function(){
+
+		$(this).attr('href', $(this).attr('data-url'));
+		$(this).removeAttr('data-url');
+		navActive = true;
+
+	});
+
+	if(curNavEltId != ''){
+	
+		markActiveNav(curNavEltId);
+
+	}
+
+}
+
+// add active class to nav element
+function markActiveNav(eltId){
+
+	navElts.removeClass('active');
+    
+    $("#"+eltId).addClass('active');
+
+}
+
+
+// custom navigation styling
+// if ($.support.pjax) {
+  
+// 	$(document).on('click', navEltsClass, function(event) {
+
+// 		console.log(navActive);
+// 		curNavElt = $(this);
+
+// 	});
+
+// }
 
 
 // $(document).on('pjax:success', function(event) {
